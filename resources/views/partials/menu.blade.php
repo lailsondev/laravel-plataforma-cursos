e34a7e57561bcf20d2d40362e71325dc1b9c939d
Lailson das Virgens <lailsondev@gmail.com>
2026-09-04 15:11:22 -0300

    feat: adiciona as views das páginas do projeto

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,9 @@
+<div class="max-w-6xl mx-auto flex justify-between items-center p-4">
+    <h1 class="text-2xl font-bold text-indigo-600"><a href="{{ route('home.index') }}">LailsonDev</a></h1>
+    <nav class="space-x-6">
+        <a href="{{ route('home.index') }}" class="hover:text-indigo-600">Início</a>
+        <a href="{{ route('courses.index') }}" class="hover:text-indigo-600">Meus Cursos</a>
+        <a href="{{ route('contact.index') }}" class="hover:text-indigo-600">Contato</a>
+        <a href="{{ route('logar.index') }}" class="hover:text-indigo-600">Logar</a>
+    </nav>
+</div>
