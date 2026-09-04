19282b2750bcfcf0a149c4356950b0ce5f5fc5f7
Lailson das Virgens <lailsondev@gmail.com>
2026-09-04 20:05:57 -0300

    chore: adiciona regra de redirecionamento nos middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        Authenticate::redirectUsing(fn () => route('logar.index'));
+        RedirectIfAuthenticated::redirectUsing(fn () => route('home.index'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
