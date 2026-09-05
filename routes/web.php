3c3055aa2993dc9cdcc202272a0568ad547b08df
Lailson das Virgens <lailsondev@gmail.com>
2026-09-05 14:42:20 -0300

    feat: adiciona rota 404

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\ErrorController;
 use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LessonController;
@@ -20,10 +21,13 @@ Route::resource('logar', LoginController::class)->only([
     'index', 'store'
 ]);
 Route::delete('/deslogar', [LoginController::class, 'destroy'])->name('logar.destroy');
-Route::get('/esqueci-senha', [ForgotPasswordController::class, 'index'])->name('forgot-password.index');
-Route::post('/esqueci-senha', [ForgotPasswordController::class, 'store'])->name('forgot-password.store');
-Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'edit'])->name('password.reset');
-Route::put('/forgot-password/', [ForgotPasswordController::class, 'update'])
-    ->middleware('guest')
-    ->name('forgot-password.update');
 
+
+Route::controller(ForgotPasswordController::class)->group(function () {
+    Route::get('/esqueci-senha', 'index')->name('forgot-password.index');
+    Route::post('/esqueci-senha', 'store')->name('forgot-password.store');
+    Route::get('/esqueci-senha/{token}', 'edit')->name('password.reset');
+    Route::put('/esqueci-senha', 'update')->name('forgot-password.update');
+})->middleware('guest');
+
+Route::fallback([ErrorController::class, 'index']);
