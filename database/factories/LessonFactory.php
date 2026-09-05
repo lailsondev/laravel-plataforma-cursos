13796e4be95babaeffa4e5a066c3db2dc7dbcf0c
Lailson das Virgens <lailsondev@gmail.com>
2026-09-05 20:03:54 -0300

    feat: adiciona novos campos slug e duration

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Lesson;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<Lesson>
@@ -17,9 +18,13 @@ class LessonFactory extends Factory
      */
     public function definition(): array
     {
+        $title = $this->faker->sentence(4);
+
         return [
-            'title' => $this->faker->sentence(4),
+            'title' => $title,
+            'slug' => Str::slug($title),
             'description' => $this->faker->paragraph(),
+            'duration' => mt_rand(60, 4500),
             'video_url' => 'https://www.youtube.com/watch?v=rqtZ0EmciJ8',
         ];
     }
